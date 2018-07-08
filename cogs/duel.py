93d61fe38bd41ea10140bebf2087e65dc3118613
zach1502 <zach1502@users.noreply.github.com>
2018-07-08 01:28:07 -0700

    More  "fun" added (#64)
    * More  "fun" added
    
    Added more words and attacks
    
    * Fixed missing commas, replaced and added an action

--- cogs/duel.py
+++ cogs/duel.py
@@ -48,7 +48,9 @@ WEAPONS = {
         'chainsaw': 6,
         'broadsword': 7,
         'katana': 4,
-        'falchion': 5
+        'falchion': 5,
+        'frying pan': 6,
+        'sickle': 3
     },
     'fire': {
         'raygun': 5,
@@ -58,11 +60,14 @@ WEAPONS = {
         'ballista': 6,
         'catapult': 5,
         'cannon': 4,
-        'mortar': 3
+        'mortar': 3,
+        'AWP': 6,
+        'AK-47': 3
     },
     'stab': {
         'naginata': 5,
-        'lance': 4
+        'lance': 4,
+        'needles': 2
     }
 }
 
@@ -74,10 +79,13 @@ SINGLE_PROJECTILE = {
         'pocket sand': 1,
         'a spear': 6,
         'a heavy rock': 3,
+        'their bedsheets': 1
     },
     'toss': {
         'a moltov cocktail': 4,
-        'a grenade': 5
+        'a grenade': 5,
+        'a flahbang': 2,
+        'a decoy grenade': 1
     }
 }
 
@@ -88,6 +96,7 @@ FAMILIAR = {
     'charge': {
         'their pet goat': 3,
         'their pet unicorn': 4,
+        'their Pikachu': 5
     },
     'constrict': {
         'their thick anaconda': 4,
@@ -98,16 +107,21 @@ SUMMON = {
     'charge': {
         'a badass tiger': 5,
         'a sharknado': 8,
-        'a starving komodo dragon': 5
+        'a starving komodo dragon': 5,
+        'a Ram f-150': 6
     },
     'swarm': {
         'all these muthafucking snakes': 5,
+        'Pokemon Go raid group': 4,
+        'Lots of bees': 4,
+        'flying fishes': 3
     }
 }
 
 MELEE = {
     'stab': {
-        'dagger': 5
+        'dagger': 5,
+        'kitchen knife': 4
     },
     'drive': {
         'fist': 4,
@@ -118,7 +132,9 @@ MELEE = {
 MARTIAL = {'roundhouse kick': 6,
            'uppercut': 5,
            'bitch-slap': 2,
-           'headbutt': 4}
+           'headbutt': 4,
+           'flying kick': 5,
+           'consecutive normal punches': 3}
 
 BODYPARTS = [
     'head',
@@ -135,7 +151,11 @@ BODYPARTS = [
     'abdomen',
     'shin',
     'knee',
-    'other knee'
+    'other knee',
+    'gluteus maximus',
+    'stomach',
+    '*back door*',
+    'nails'
 ]
 
 VERB_IND_SUB = {'munch': 'munches', 'toss': 'tosses'}
@@ -163,18 +183,22 @@ CRITICAL = {"Quicker than the eye can follow, {a} delivers a devastating blow wi
 HEALS = {
     'inject': {
         'morphine': 4,
-        'nanomachines': 5
+        'nanomachines': 5,
+        'more blood': 2
     },
     'smoke': {
         'a fat joint': 2,
         'medicinal incense': 3,
-        'their hookah': 3
+        'their hookah': 3,
+        'a cigar': 1,
+        'a cigarette': 1
     },
     'munch': {
         'on some': {
             'cake': 5,
             'cat food': 3,
-            'dog food': 4
+            'dog food': 4,
+            'pills': 2
         },
         'on a': {
             'waffle': 4,
@@ -187,15 +211,18 @@ HEALS = {
             'unicorn piss': 5,
             'purple drank': 2,
             'sizzurp': 3,
-            'goon wine': 2
+            'goon wine': 2,
+            'soda': 1
         },
         'a': {
             'generic hp potion': 5,
             'refreshingly delicious can of 7-Up': 3,
-            'fresh mug of ale': 3
+            'fresh mug of ale': 3,
+            'nuka cola': 4
         },
         'an': {
-            'elixir': 5
+            'elixir': 5,
+            'energy drink': 3
         }
     }
 }
