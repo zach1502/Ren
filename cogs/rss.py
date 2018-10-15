9e667d9d748b4bc75b91511bd57c43bdd5b758a0
Lui <injabie3@gmail.com>
2018-10-14 20:54:33 -0700

    [RSS] Add logging; Pylint file (#116)
    * [Pylint] Unignore rss.py
    
    * [RSS] Add logger, change prints to use logger
    
    * [RSS] Add logging import, fix bad whitespaces
    
    * [RSS] Fix import order, move module docstring to top
    
    * [RSS] Add function docstrings
    
    * [RSS] Fix function names to conform with Pylint
    
    * [RSS] Fix attribute names to conform with Pylint
    
    * [RSS] Fix variable names to conform with Pylint
    
    * [RSS] Fix argument names to conform with Pylint
    
    * [RSS] Change methods to functions, since no self reference
    
    * [RSS] camelCase function names
    
    * [RSS] dict -> myDict; remove inherit from class; remove else return
    
    * [RSS] Fix long line; Add class docstring; Make length check pythonic
    
    * [RSS] Better variable names for dictionaries

--- cogs/rss.py
+++ cogs/rss.py
@@ -1,213 +1,221 @@
-import discord
-from discord.ext import commands
-from __main__ import send_cmd_help
-from cogs.utils.dataIO import dataIO
-# DEBUG
-import os
-os.environ['PYTHONASYNCIODEBUG'] = '1'
-# DEBUG
-import asyncio
-import aiohttp
-import feedparser
-from datetime import datetime
-from bs4 import BeautifulSoup
-
 """
 Cogs Purpose: To be able to hook up any RSS feed(s) to the bot and post it in a channel
-Requirements: 
+Requirements:
     - sudo pip install bs4
     - sudo pip install feedparser
 """
-#---------------------------------------------------------------------------------------------#
+import asyncio
+from datetime import datetime
+import logging
+import os
+
+import aiohttp
+import discord
+from discord.ext import commands
+import feedparser
+from bs4 import BeautifulSoup
+from cogs.utils.dataIO import dataIO
+from __main__ import send_cmd_help # pylint: disable=no-name-in-module
+
+LOGGER = None
+RSS_IMAGE = ("https://upload.wikimedia.org/wikipedia/en/thumb/4/43/Feed-icon.svg/"
+             "1200px-Feed-icon.svg.png")
+
 def date2epoch(date):
+    """Converts a datetime date into epoch for storage in JSON."""
     try:
-        epoch = datetime.strptime(date,'%a, %d %b %Y %H:%M:%S %z').timestamp()
+        epoch = datetime.strptime(date, '%a, %d %b %Y %H:%M:%S %z').timestamp()
     except ValueError:
-        epoch = datetime.strptime(date,'%a, %d %b %Y %H:%M:%S %Z').timestamp()
+        epoch = datetime.strptime(date, '%a, %d %b %Y %H:%M:%S %Z').timestamp()
         return epoch
-        
+
     return epoch
 
-#---------------------------------------------------------------------------------------------#
 def epoch2date(epoch):
+    """Converts an epoch time into a datetime date from storage in JSON."""
     date = datetime.fromtimestamp(epoch).strftime('%a, %d %b %Y %I:%M%p')
     return date
- 
-#---------------------------------------------------------------------------------------------#
-def check_filesystem():
 
+def checkFilesystem():
+    """Check if the folders/files are created."""
     folders = ("data/rss")
     for folder in folders:
         if not os.path.exists(folder):
             print("RSS: Creating folder: {} ...".format(folder))
             os.makedirs(folder)
-            
+
     files = ("data/rss/config.json", "data/rss/feeds.json")
     for file in files:
         if not os.path.exists(file):
             print("RSS: Creating file: {} ...".format(file))
-          
+
             if "feeds" in file:
                 #build a default feeds.json
-                dict = {}
-                default_feed = {}
-                default_feed['id'] = 0
-                default_feed['latest_post_time'] = 0
-                dict['feeds'] = []
-                dict['feeds'].append(default_feed)
-                dataIO.save_json("data/rss/feeds.json",dict)
+                defaultDict = {}
+                defaultFeedItem = {}
+                defaultFeedItem['id'] = 0
+                defaultFeedItem['latest_post_time'] = 0
+                defaultDict['feeds'] = []
+                defaultDict['feeds'].append(defaultFeedItem)
+                dataIO.save_json("data/rss/feeds.json", defaultDict)
             elif "config" in file:
                 #build a default config.json
-                dict = {}
-                dict['post_channel'] = "change_me"
-                dict['rss_feed_urls'] = ["change_me"]
-                dict['check_interval'] = 3600 #default to checking every hour
-                dataIO.save_json("data/rss/config.json",dict)
+                defaultDict = {}
+                defaultDict['post_channel'] = "change_me"
+                defaultDict['rss_feed_urls'] = ["change_me"]
+                defaultDict['check_interval'] = 3600 #default to checking every hour
+                dataIO.save_json("data/rss/config.json", defaultDict)
 
-#---------------------------------------------------------------------------------------------#                
-class RSSFeed(object):
+def _getFeed(rssUrl, channel, index=None):
+
+    if channel is None:
+        return []
+
+    feeds = dataIO.load_json("data/rss/feeds.json")
+
+    #ensure every rss url has a specified id and most recent post epoch
+    try:
+        latestPostTime = feeds['feeds'][index]['latest_post_time']
+    except IndexError:
+        feedDict = {}
+        feedDict['id'] = index
+        feedDict['latest_post_time'] = 0
+        feeds['feeds'].append(feedDict)
+        dataIO.save_json("data/rss/feeds.json", feeds)
+        feeds = dataIO.load_json("data/rss/feeds.json")
+        latestPostTime = feeds['feeds'][index]['latest_post_time']
+
+    news = []
+    feed = feedparser.parse(rssUrl)
+
+    for item in feed['items']:
+        itemPostTime = date2epoch(item['published'])
+        if _isNewItem(latestPostTime, itemPostTime):
+            rssItem = {}
+            rssItem['title'] = item['title']
+            rssItem['link'] = item['link']
+            rssItem['published'] = item['published']
+            rssItem['summary'] = item['summary']
+            rssItem['url'] = rssUrl
+            news.append(rssItem)
+
+    if not news:
+        LOGGER.info("No new items in feed %s", str(index))
+    else:
+        LOGGER.info("%s new items in feed %s", len(news), str(index))
+
+    latestPostTime = _getLatestPostTime(feed['items'])
+    if latestPostTime:
+        feeds['feeds'][index]['latest_post_time'] = latestPostTime
+    dataIO.save_json("data/rss/feeds.json", feeds)
+
+    return news
+
+def _getLatestPostTime(feedItems):
+    publishedTimes = []
+    for item in feedItems:
+        publishedTimes.append(date2epoch(item['published']))
+    if publishedTimes:
+        return max(publishedTimes)
+    return None #lets be explicit :)
+
+def _isNewItem(latestPostTime, itemPostTime):
+    return latestPostTime < itemPostTime
+
+class RSSFeed():
+    """RSS cog"""
     def __init__(self, bot):
         self.settings = dataIO.load_json("data/rss/config.json")
         self.bot = bot
-        self.rss_feed_urls = self.settings['rss_feed_urls']
-        self.check_interval = self.settings['check_interval']
-        self.channel_id = self.settings['post_channel']
-        
-#---------------------------------------------------------------------------------------------#
-    def _is_new_item(self,latest_post_time, item_post_time):
-        return latest_post_time < item_post_time
-        
-#---------------------------------------------------------------------------------------------#   
-    def _get_latest_post_time(self, feed_items):
-        published_times = []
-        for item in feed_items:
-            published_times.append(date2epoch(item['published']))
-        if published_times:
-            return max(published_times)
-        else:
-            return None #lets be explicit :)
+        self.rssFeedUrls = self.settings['rss_feed_urls']
+        self.checkInterval = self.settings['check_interval']
+        self.channelId = self.settings['post_channel']
 
-#---------------------------------------------------------------------------------------------#
     @commands.group(name="rss", pass_context=True, no_pm=True)
     async def _rss(self, ctx):
         """Utilities for the RSS cog"""
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
 
-#---------------------------------------------------------------------------------------------#            
     @_rss.command(pass_context=True, no_pm=True)
-    async def setinterval(self,ctx):
+    async def setinterval(self, ctx):
         """Set the interval for rss to scan for updates"""
         pass
-#---------------------------------------------------------------------------------------------#   
-    def _get_feed(self, rss_url, channel, index=None): 
-        
-        if channel is None:
-            return []
-        
-        feeds = dataIO.load_json("data/rss/feeds.json")
-        
-        #ensure every rss url has a specified id and most recent post epoch
-        try:
-            latest_post_time = feeds['feeds'][index]['latest_post_time']
-        except IndexError:
-            dict = {}
-            dict['id'] = index
-            dict['latest_post_time'] = 0
-            feeds['feeds'].append(dict)
-            dataIO.save_json("data/rss/feeds.json",feeds)
-            feeds = dataIO.load_json("data/rss/feeds.json")
-            latest_post_time = feeds['feeds'][index]['latest_post_time']
-        
-        news = []
-        feed = feedparser.parse(rss_url)
-        
-        for item in feed['items']:
-            item_post_time = date2epoch(item['published'])
-            if self._is_new_item(latest_post_time, item_post_time):
-                dict = {}
-                dict['title'] = item['title']
-                dict['link'] = item['link']
-                dict['published'] = item['published']
-                dict['summary'] = item['summary']
-                dict['url'] = rss_url
-                news.append(dict)
-                
-        if len(news) == 0:
-            print("RSS: no new items in feed {}".format(str(index)))
-        else:
-            print("RSS: {} new items in feed {}".format(len(news), str(index)))
-        
-        latest_post_time = self._get_latest_post_time(feed['items'])
-        if latest_post_time is not None:
-            feeds['feeds'][index]['latest_post_time'] = latest_post_time
-        dataIO.save_json("data/rss/feeds.json", feeds)
-        
-        return news
-        
-#---------------------------------------------------------------------------------------------#       
-    async def rss(self):
-        """ Checks for rss updates periodically and posts any new content to the specific channel"""
-        
+
+    async def rss(self): # pylint: disable=too-many-locals
+        """RSS background checker.
+        Checks for rss updates periodically and posts any new content to the specific
+        channel.
+        """
+
         while self == self.bot.get_cog("RSSFeed"):
-            print("------------------------------------")
-            print("RSS: scanning feed(s) for updates...")
-            print("------------------------------------")
-            
-            post_channel = self.bot.get_channel(self.channel_id)
+            LOGGER.info("Scanning feed(s) for updates...")
+
+            postChannel = self.bot.get_channel(self.channelId)
             updates = []
             idx = 0
-            
-            if post_channel is None:
-                print("RSS: Can't find channel, Bot is not yet logged in.")
-            
-            for feed_url in self.rss_feed_urls:
-                feed_updates = self._get_feed(feed_url, post_channel, index=idx)
-                updates += feed_updates
+
+            if not postChannel:
+                LOGGER.error("Can't find channel: bot is not logged in yet.")
+
+            for feedUrl in self.rssFeedUrls:
+                feedUpdates = _getFeed(feedUrl, postChannel, index=idx)
+                updates += feedUpdates
                 idx += 1
-            
-            #reversed so updates display from latest to earliest, since they are appended earliest to latest
-            for item in reversed(updates): 
+
+            # Reversed so updates display from latest to earliest, since they are
+            # appended earliest to latest.
+            for item in reversed(updates):
                 embed = discord.Embed()
                 embed.colour = discord.Colour.orange()
                 embed.title = item['title']
                 embed.url = item['link']
-                
+
                 async with aiohttp.ClientSession() as session:
                     async with session.get(item['link'].replace(' ', '%20')) as resp:
                         page = await resp.text()
-                
+
                 soup = BeautifulSoup(page, "html.parser")
                 try:
-                    image_url = soup.find("meta", property="og:image")['content']
-                    embed.set_image(url=image_url)
+                    imageUrl = soup.find("meta", property="og:image")['content']
+                    embed.set_image(url=imageUrl)
                 except (KeyError, TypeError):
                     pass
-                
+
                 #ugly, but want a nicer "human readable" date
-                formatted_date = epoch2date(date2epoch(item['published']))
-                embed.add_field(name="Date Published", value=formatted_date, inline=False)
-                
+                formattedDate = epoch2date(date2epoch(item['published']))
+                embed.add_field(name="Date Published", value=formattedDate, inline=False)
+
                 html2text = BeautifulSoup(item['summary'], "html.parser").get_text()
                 embed.add_field(name="Summary", value=html2text, inline=False)
-                
-                footer_text = "This update is from {}".format(item['url'])
-                rss_image = "https://upload.wikimedia.org/wikipedia/en/thumb/4/43/Feed-icon.svg/1200px-Feed-icon.svg.png"
-                embed.set_footer(text=footer_text, icon_url=rss_image)
-                
-                await self.bot.send_message(post_channel, embed=embed)
-                
-            try:
-                await asyncio.sleep(self.check_interval)
-            except asyncio.CancelledError as e:              
-                print("borked")                        
-                raise e
-            
-#---------------------------------------------------------------------------------------------# 
-def setup(bot):
-    #check_filesystem()
-    rss_obj = RSSFeed(bot)
-    bot.add_cog(rss_obj)
-    bot.loop.create_task(rss_obj.rss())
 
-#---------------------------------------------------------------------------------------------#
+                footerText = "This update is from {}".format(item['url'])
+                rssImage = RSS_IMAGE
+                embed.set_footer(text=footerText, icon_url=rssImage)
+
+                await self.bot.send_message(postChannel, embed=embed)
+
+            try:
+                await asyncio.sleep(self.checkInterval)
+            except asyncio.CancelledError as error:
+                LOGGER.error("The asyncio sleep was cancelled!")
+                LOGGER.error(error)
+                raise error
+
+def setup(bot):
+    """Add the cog to the bot."""
+    #checkFilesystem()
+    global LOGGER # pylint: disable=global-statement
+    LOGGER = logging.getLogger("red.RSSFeed")
+    if LOGGER.level == 0:
+        # Prevents the LOGGER from being loaded again in case of module reload.
+        LOGGER.setLevel(logging.INFO)
+        handler = logging.FileHandler(filename="data/rss/info.log",
+                                      encoding="utf-8",
+                                      mode="a")
+        handler.setFormatter(logging.Formatter("%(asctime)s %(message)s",
+                                               datefmt="[%d/%m/%Y %H:%M:%S]"))
+        LOGGER.addHandler(handler)
+    rssCog = RSSFeed(bot)
+    bot.add_cog(rssCog)
+    bot.loop.create_task(rssCog.rss())
