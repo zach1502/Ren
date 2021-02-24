327df208afe4e7251582b23635d932a9c1fe3b20
tplamondon <tplamondon@tutanota.de>
2021-02-23 22:03:24 -0800

    [Source] Handle case where anime is not found (#359)
    * Updated source cog's description and hopefully deal with null stirng values returned from trace.moe api
    
    * removed funciton in favour of another method
    
    * Extracted message building to it's own function to reduce duplication
    
    * eilasource url https://cdn.discordapp.com/attachments/804124733545250876/812030228100677693/unknown.png
    
    * added % after the similarity message, as well as converted to f strings
    
    * Adjusted docstring to be on 1 line

--- cogs/source/source.py
+++ cogs/source/source.py
@@ -3,39 +3,40 @@ import tracemoepy
 from tracemoepy.errors import EmptyImage, EntityTooLarge, ServerError, TooManyRequests
 
 
+def messageBuilder(titleEnglish: str, anilistID: str, episode: str, similarity: int):
+    """Builds the message that is sent in response"""
+    # If the title isn't found, just return no matching anime found
+    if titleEnglish == "No Title Found":
+        return "No matching anime found.\nThis could be because provided picture isn't a screenshot of an anime, or the episode it's from is too new"
+    # Title of anime
+    message = f"Anime: {titleEnglish}"
+    # episode number
+    message += f"\nEpisode: {episode}"
+    # How similar message is
+    message += f"\nSimilarity: {similarity*100:.3f}%"
+    if similarity * 100 < 90:
+        # warning if similarity is less than 90%
+        message += "\nWARNING: Similarity less than 90%, result may not be accurate"
+    if anilistID != "No anilistID Found":
+        # URL
+        message += f"\nhttps://anilist.co/anime/{anilistID}"
+    return message
+
+
 async def postSourceFunction(ctx, imageURL):
     """helper method"""
     try:
+        # use the API to get results
         tracemoe = tracemoepy.tracemoe.TraceMoe()
         result = tracemoe.search(imageURL.strip("<>"), is_url=True)
-        titleEnglish = result.docs[0].title_english
-        anilistID = f"{result.docs[0].anilist_id}"
-        episode = f"{result.docs[0].episode}"
-        similarity = float(result.docs[0].similarity)
-        URL = "https://anilist.co/anime/" + anilistID
+        titleEnglish = result.docs[0].title_english or "No Title Found"
+        anilistID = f"{result.docs[0].anilist_id}" or "No anilistID Found"
+        episode = f"{result.docs[0].episode}" or "No Episode Found"
+        similarity = float(result.docs[0].similarity) or 0
 
-        if similarity < 0.8:
-            await ctx.send(
-                "Anime: "
-                + titleEnglish
-                + "\nEpisode: "
-                + episode
-                + "\nWARNING: Similarity less than 80%, result may not be accurate"
-                + "\n"
-                + URL
-            )
-        else:
-            await ctx.send(
-                "Anime: "
-                + titleEnglish
-                + "\nSimilarity: "
-                + ("%.3f" % ((similarity) * 100))
-                + "%"
-                + "\nEpisode: "
-                + episode
-                + "\n"
-                + URL
-            )
+        # send the message using messageBGuilder to build the message
+        await ctx.send(messageBuilder(titleEnglish, anilistID, episode, similarity))
+
     except TooManyRequests:
         await ctx.send("Please try again later")
     except EntityTooLarge:
@@ -53,7 +54,7 @@ async def postSourceFunction(ctx, imageURL):
 class Source(commands.Cog):
     @commands.group(name="source", invoke_without_command=True)
     async def sourceCommand(self, ctx):
-        """Looks for source of image
+        """Looks for source of a screenshot from anime, using trace.moe
 
         Parameters:
         -----------
@@ -70,7 +71,7 @@ class Source(commands.Cog):
 
     @sourceCommand.command(name="url")
     async def urlSource(self, ctx, imageURL):
-        """Looks for source of image
+        """Looks for source of a screenshot from anime, using trace.moe
 
         Parameters:
         -----------
