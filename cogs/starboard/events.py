1a991b2f2f98573afbc27ff91efae9a584de6524
Injabie3 <injabie3@gmail.com>
2021-09-25 17:59:15 -0700

    [Starboard] Check role and channel denylists separately
    - If either a role or channel are on the deny list, do not star.
    - This caused problems when there was a role on the allowlist, which
      returned True, but the channel was on the deny list, so ORing True
      from the role check with False from the channel check resulted in
      someone being able to star a message from a channel that was blocked.

--- cogs/starboard/events.py
+++ cogs/starboard/events.py
@@ -170,10 +170,11 @@ class StarboardEvents:
             return
         if not starboard.enabled:
             return
-        allowed = starboard.check_roles(member)
-        allowed |= starboard.check_channel(self.bot, channel)
-        if not allowed:
-            log.debug("User or channel not in allowlist")
+        if not starboard.check_roles(member):
+            log.debug("User not in allowlist")
+            return
+        if not starboard.check_channel(self.bot, channel):
+            log.debug("Channel not in allowlist")
             return
 
         star_channel = guild.get_channel(starboard.channel)
